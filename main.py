eda0bbc7cf6ee495f44a87898db1782ead85c2be
Kevin Bao <kevinbao03@gmail.com>
2022-05-07 16:19:45 -0700

    Added While Loop

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(27, GPIO.OUT)
 
-GPIO.output(27, True)
+while True:
+    GPIO.output(27, True)
+    time.sleep(1.5)
+    GPIO.output(27, False)
 
 
 
